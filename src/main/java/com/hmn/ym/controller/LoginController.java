21af8ee89dc8c9a4419bab265451575cde919948
oymw20170123@163.com <oymw20170123@163.com>
2019-08-05 22:34:22 +0800

    0805修改

--- src/main/java/com/hmn/ym/controller/LoginController.java
+++ src/main/java/com/hmn/ym/controller/LoginController.java
@@ -1,5 +1,6 @@
 package com.hmn.ym.controller;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
@@ -8,6 +9,12 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.hmn.ym.utils.Const;
+import com.hmn.ym.utils.SpringUtils;
+import com.hmn.ym.utils.des.DesEncrypt;
+import com.hmn.ym.utils.des.MD5Utils;
+import com.hmn.ym.utils.result.JsonResult;
 
 @Controller
 public class LoginController extends BaseController{
@@ -23,13 +30,40 @@ public class LoginController extends BaseController{
 	private static final String user_login_view = "user/userIndex";
 	
 	
-	@RequestMapping("login")
+	@RequestMapping("index.do")
 	public String index(HttpServletRequest request, HttpServletResponse response) {
 		logger.info("login");
 		return login_view;
 	}
 	
+	@RequestMapping("login.do")
+	public void login(HttpServletRequest request, HttpServletResponse response) {
+
+		Map<String, String> param = this.getParameters(request);
+		String code = "";
+		String msg = "";
+			if (!param.containsKey("userAccount")) {
+				code = "300";
+				msg = "用户名不能为空！！！";
+			}
+			if (!param.containsKey("userPassword")) {
+				code = "300";
+				msg = "密码不能为空！！！";
+			}
+			String userAccount = param.get("userAccount");
+//			DesEncrypt desEncrpt = new DesEncrypt(Const.DES_PUBLIC_ENCRYPT_KEY);
+//			DesEncrypt aesEncrypt = new DesEncrypt(Const.DES_PRIVATE_ENCRYPT_KEY);
+//			String userPassword = desEncrpt.decrypt(param.get("userPassword"));
+//			String checkpassword = MD5Utils.stringToMD5(aesEncrypt.encrypt(userPassword));
+			Map<String,String> params = new HashMap<String,String>();
+			params.put("userPhone", userAccount);
+			
+						code = JsonResult.SUCCESS;
+						msg = "登录成功";
+		System.out.println(msg);
+		SpringUtils.renderJsonResult(response, code, msg);
 	
+	}
 	
 	/**
 	 * 进入个人主页
--- src/main/java/com/hmn/ym/controller/RegisterController.java
+++ src/main/java/com/hmn/ym/controller/RegisterController.java
@@ -1,7 +1,9 @@
 package com.hmn.ym.controller;
 
+import java.util.HashMap;
 import java.util.Map;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -10,6 +12,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.hmn.ym.utils.Const;
+import com.hmn.ym.utils.des.DesEncrypt;
 
 /**
  * 用户注册信息
@@ -39,7 +42,7 @@ public class RegisterController extends BaseController{
 	/**
 	 * 用户注册成功后跳转用户页面
 	 */
-	public static final String userIndex = "userindex";
+	public static final String userIndex = "user/userIndex";
 	
 	/**
 	 * 注册协议
@@ -72,8 +75,40 @@ public class RegisterController extends BaseController{
 	@RequestMapping(value = "toNext.do")
 	public String toNext(HttpServletRequest request, HttpServletResponse response, Model model) {
 		Map<String, String> param = this.getParameters(request);
-		
-		
+		if (!param.containsKey("userPhone")) {
+			model.addAttribute("message", "手机号不能为空");
+			return register;
+		}
+		if (!isPhone(param.get("userPhone"))) {
+			model.addAttribute("message", "手机号格式错误");
+			return register;
+		}
+		String phoneNum= param.get("userPhone");
+		Map<String,String> params = new HashMap<String,String>();
+		params.put("userPhone", phoneNum);
+		DesEncrypt desEncrypt = new DesEncrypt();
+		String smsCode = null;
+		Cookie[] cookie = request.getCookies();
+		for (int i = 0; i < cookie.length; i++) {
+			Cookie cook = cookie[i];
+			if (cook.getName().equalsIgnoreCase("smsRand")) { // 获取键
+				smsCode = desEncrypt.decrypt(cook.getValue().toString()); // 获取值
+				break;
+			}
+		}
+		if ("".equals(smsCode) || null == smsCode) {
+			model.addAttribute("message", "验证码过期");
+			return register;
+		}
+		if (!smsCode.equals(param.get("smsCode"))) {
+			model.addAttribute("message", "验证码错误");
+			return register;
+		}
+		model.addAttribute("inviteUserid", param.get("u"));
+		model.addAttribute("userPhone", param.get("userPhone"));
+		model.addAttribute("userAccount", param.get("userAccount"));
+		model.addAttribute("publickey", Const.DES_PUBLIC_ENCRYPT_KEY);
+	
 		return register2;
 	}
 	
