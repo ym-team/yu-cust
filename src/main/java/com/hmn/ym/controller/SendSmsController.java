6c2082cc369400c0f41ac17f07496fc940edee7c
oymw20170123@163.com <oymw20170123@163.com>
2019-08-05 22:32:15 +0800

    验证码

--- src/main/java/com/hmn/ym/controller/SendSmsController.java
+++ src/main/java/com/hmn/ym/controller/SendSmsController.java
@@ -1,7 +1,5 @@
 package com.hmn.ym.controller;
 
-import java.util.Random;
-
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
